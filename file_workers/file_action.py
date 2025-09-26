ca14e9a7c7751ec04d81cfac574f959e0e649a08
SashoAlex2000 <sahooo3@abv.bg>
2025-09-26 22:43:40 +0300

    feat: add abstract base class for perforing an action on a file

---
+++ file_workers/file_action.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class FileAction(ABC):
+
+    """
+    Abstract Base Class, providing a reliable blueprint method for working with files
+    """
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def perform_action_on_file(self, path_to_file: str, *args, **kwargs) -> Any | None:
+
+        """
+        :param path_to_file: str, (valid) abs path to a local file
+        :param args:
+        :param kwargs:
+        :return: Any or None -> up to the concrete implementation of a concrete child class
+        """
+
+        pass
